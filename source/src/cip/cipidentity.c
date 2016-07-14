8b9737830b4f5b20fc82e2d5ea9e67fe137acac1
Tim O'Callaghan <tocallaghan@meyn.net>
2016-07-14 15:49:39 +0200

    upstream OpENer seems to have picked up. trees while the same, have diverged in git from initial commit probably due to line endings. This is the rebased version of toc-opener to track upstream commits, the current commit is the ported patch from - Perry Kundert <perry@hardconsulting.com> Fixed compilations warnings regarding char/EIP_BYTE types

--- source/src/cip/cipidentity.c
+++ source/src/cip/cipidentity.c
@@ -43,7 +43,7 @@ CipRevision revision_ = { OPENER_DEVICE_MAJOR_REVISION,
 EipUint16 status_ = 0; /**< Attribute 5: Status */
 EipUint32 serial_number_ = 0; /**< Attribute 6: Serial Number, has to be set prior to OpENer initialization */
 CipShortString product_name_ = { sizeof(OPENER_DEVICE_NAME) - 1,
-    OPENER_DEVICE_NAME }; /**< Attribute 7: Product Name */
+    (EipByte *) OPENER_DEVICE_NAME }; /**< Attribute 7: Product Name */
 
 /** Private functions, sets the devices serial number
  * @param serial_number The serial number of the device
