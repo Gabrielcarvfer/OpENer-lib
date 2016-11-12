c145ad8e0d45852748c61474261ab52cd1459352
Gabriel Ferreira <gabrielcarvfer@gmail.com>
2016-11-12 00:58:30 -0200

    Started to rebuild OpENer interface

---
+++ source/src/cip/Opener_IOConnection.h
@@ -0,0 +1,16 @@
+//
+// Created by gabriel on 11/11/2016.
+//
+
+#ifndef OPENER_OPENER_IOCONNECTION_H
+#define OPENER_OPENER_IOCONNECTION_H
+
+
+class Opener_IOConnection
+{
+    Opener_IOConnection() = delete;
+    ~Opener_IOConnection ();
+};
+
+
+#endif //OPENER_OPENER_IOCONNECTION_H
